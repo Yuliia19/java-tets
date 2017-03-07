ccb406b090b0ce530a88c7b30a757cac0b5432ee
Yuliia19 <julia.mcmahon94@gmail.com>
2017-03-07 12:01:12 +0200

    Update First.java

--- src/epam/firstProject/First.java
+++ src/epam/firstProject/First.java
@@ -4,5 +4,6 @@ public class First {
 public static void main(String []args){
 	System.out.println("Hello world");
 	System.out.println("Github is cool");
+		System.out.println("Github still is cool");
 }
 }
